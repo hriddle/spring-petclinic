abc3c0eaa24a053cf7e7d870952795354bd747a4
Heather Riddle <heather0809@gmail.com>
2019-03-07 16:23:08 -0600

    Add cookbook

---
+++ cookbooks/first_cookbook/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook:: first_cookbook
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+
+file "#{ENV['HOME']}/test.txt" do
+  content '!!!!! This file was created by Heather !!!!!'
+end
